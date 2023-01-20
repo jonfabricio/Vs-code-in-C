a3eee71f5d3a90fc89fbf3939f65f59735b5b875
Jojow-World <jonatas-fab@hotmail.com>
2023-01-20 06:35:55 +0000

    novos comandos

--- fogefoge.c
+++ fogefoge.c
@@ -1,93 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fogefoge.h"
+#include "mapa.h"
 
-char** mapa;
-int linhas;
-int colunas;
+MAPA m;
+POSICAO heroi;
 
-void lemapa() {
-	FILE* f;
-	f = fopen("mapa.txt", "r");
-	if(f == 0) {
-		printf("Erro na leitura do mapa");
-		exit(1);
-	}
-
-	fscanf(f, "%d %d", &linhas, &colunas);
-	alocamapa();
-	
-	for(int i = 0; i < 5; i++) {
-		fscanf(f, "%s", mapa[i]);
-	}
-
-	fclose(f);
-}
-
-void alocamapa() {
-	mapa = malloc(sizeof(char*) * linhas);
-
-	for(int i = 0; i < linhas; i++) {
-		mapa[i] = malloc(sizeof(char) * colunas + 1);
-	}
-}
-
-void liberamapa() {
-	for(int i = 0; i < linhas; i++) {
-		free(mapa[i]);
-	}
-
-	free(mapa);
-}
 
 int acabou() {
 	return 0;
 }
 
 void move(char direcao) {
-	int x;
-	int y;
+    
+    if(
+        direcao != 'a' && 
+        direcao != 'w' &&
+        direcao != 's' &&
+        direcao != 'd'
+    ) return;
 
-	for(int i = 0; i < linhas; i++) {
-		for(int j = 0; j < colunas; j++) {
-			if(mapa[i][j] == '@') {
-				x = i;
-				y = j;
-				break;
-			}
-		}
-	} 
+      
 
 	switch(direcao) {
 		case 'a':
-			mapa[x][y-1] = '@';
+			m.matriz[heroi.x][heroi.y-1] = '@';
+			heroi.y--;
 			break;
 		case 'w':
-			mapa[x-1][y] = '@';
+			m.matriz[heroi.x-1][heroi.y] = '@';
+			heroi.x--;
 			break;
 		case 's':
-			mapa[x+1][y] = '@';
+			m.matriz[heroi.x+1][heroi.y] = '@';
+			heroi.x++;
 			break;
 		case 'd':
-			mapa[x][y+1] = '@';
+			m.matriz[heroi.x][heroi.y+1] = '@';
+			heroi.y++;
 			break;
 	}
-
-	mapa[x][y] = '.';
-}
-
-void imprimemapa() {
-	for(int i = 0; i < linhas; i++) {
-		printf("%s\n", mapa[i]);
-	}
+	
 }
 
 int main() {
 	
-	lemapa();
+	lemapa(&m);
+	encontramapa(&m, &heroi, '@');
 
 	do {
-		imprimemapa();
+		imprimemapa(&m);
 
 		char comando;
 		scanf(" %c", &comando);
@@ -96,5 +58,5 @@ int main() {
 
 	} while (!acabou());
 
-	liberamapa();
+	liberamapa(&m);
 }
